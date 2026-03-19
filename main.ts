d8463c5c9ab45f44c1941e224356d949b5a31db6
pindakaas0407 <maximerooman1@gmail.com>
2026-03-19 09:21:03 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
